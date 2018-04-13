80f3258ff122abe708ae37aa5edf411af16f8794
Kate Fulton <kfulton@marchex.com>
2018-04-13 13:31:47 -0700

    WIP: Keyword enum

--- src/main/scala/kfulton/nand2tetris2/analyzer/tokenizer/Token.scala
+++ src/main/scala/kfulton/nand2tetris2/analyzer/tokenizer/Token.scala
@@ -8,5 +8,22 @@ case class TokenInt(int: Int) extends Token //regex
 case class TokenString(string: String) extends Token //regex
 case class Identifier(alphaNumeric: String) extends Token //regex
 
-sealed abstract class KeywordValue(name: String)
-case object Class extends KeywordValue("class")
+sealed abstract class KeywordValue extends CanonicalName {
+  def values = Vector(Class)
+}
+
+case object Class extends KeywordValue {
+  override val canonical: String = "class"
+}
+
+trait CanonicalName {
+  val canonical: String
+}
+
+trait NamedEnum[T <: CanonicalName] {
+  def values: Vector[T]
+
+  def forName(name: String): Option[T] =
+    values.find(_.canonical.equalsIgnoreCase(name.trim))
+}
+
